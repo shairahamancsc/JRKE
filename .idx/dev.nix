7cf91f3f8ef4a10645f3a28302948b86416c88a9
Shaik Anisur Rahaman <sskingshaikpro@gmail.com>
2025-05-27 11:47:19 +0000

    prisma:warn Prisma failed to detect the libssl/openssl version to use, and may not work as expected. Defaulting to "openssl-1.1.x". Please manually install OpenSSL and try installing Prisma again. Environment variables loaded from .env
    prisma:warn Prisma failed to detect the libssl/openssl version to use, and may not work as expected. Defaulting to "openssl-1.1.x".
    Please manually install OpenSSL and try installing Prisma again.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,7 +7,7 @@
   packages = [
     pkgs.nodejs_20
     pkgs.zulu
-    pkgs.pnpm
+    pkgs.pnpm # orpnpm add prisma --save-dev
   ];
   # Sets environment variables in the workspace
   env = {};
---
+++ prisma/migrations/20250527114231_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Example" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "Example_pkey" PRIMARY KEY ("id")
+);
